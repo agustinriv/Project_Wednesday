49a8871d32d1ed7d374e0d16fa3733feb557afd0
agustinriv <rivas.ae97@gmail.com>
2025-10-18 01:23:08 -0300

    Clase02

--- main.py
+++ main.py
@@ -1,15 +1,47 @@
 import pandas as pd
+import os
+import datetime
+import logging
 
+from src.loader import cargar_datos
+from src.features import feature_engineering_lag
+
+## config basico logging
+os.makedirs("logs", exist_ok=True)
+
+fecha = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+nombre_log = f"log_{fecha}.log"
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(name)s %(lineno)d - %(message)s',
+    handlers=[
+        logging.FileHandler(f"logs/{nombre_log}", mode="w", encoding="utf-8"),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(__name__)
+
+## Funcion principal
 def main():
+    logger.info(f"Inicio de ejecución")
     print("Inicio de ejecución")
 
-    # Cargar dataset
-    df = pd.read_csv("datasets/data_test.csv")
-    print(df.head())
-    print(f"Filas: {df.shape[0]}, Columnas: {df.shape[1]}")
+    #00 Cargar dataset
+    os.makedirs("datasets", exist_ok=True)
+    path = "datasets/competencia_01_test.csv"
+    df = cargar_datos(path)
 
-    with open("logs/logs.txt", "a") as f:
-        f.write(f"Dataset cargado con {df.shape[0]} filas y {df.shape[1]} columnas\n")
+    #01 Lags
+    atributos = ["mrentabilidad"]
+    cant_lag = 2
+    df = feature_engineering_lag(df, columnas=atributos, cant_lag=cant_lag)
+  
+    #02 Guardar datos
+    path = "datasets/competencia_01_lag.csv"
+    df.to_csv(path, index=False)
+
+    logger.info(f">>> Ejecución finalizada. Revisar logs para más detalle. {nombre_log}")
 
 if __name__ == "__main__":
     main()
